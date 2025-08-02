6587c1f35833d6375cf3b63c46d6fd7961d3a02c
Rohan Shrestha <103275068+sthar2820@users.noreply.github.com>
2025-08-02 11:45:23 -0400

    Update saver.py

--- utils/saver.py
+++ utils/saver.py
@@ -1,6 +1,4 @@
-import os
 import pandas as pd
 
-def save_to_excel(df, output_path):
-    os.makedirs(os.path.dirname(output_path), exist_ok=True)
-    df.to_excel(output_path, index=False)
+def save_to_excel(df, out_path):
+    df.to_excel(out_path, index=False)
