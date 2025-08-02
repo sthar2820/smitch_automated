8019df40a20e295d12c2c9d8eeec48d346f0e273
Rohan Shrestha <103275068+sthar2820@users.noreply.github.com>
2025-08-02 11:30:28 -0400

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,15 @@
+import json
+import os
+
+LOG_PATH = "logs/processed_files.json"
+
+def load_processed_log():
+    if os.path.exists(LOG_PATH):
+        with open(LOG_PATH, "r") as f:
+            return json.load(f)
+    return {}
+
+def update_log(new_log):
+    with open(LOG_PATH, "w") as f:
+        json.dump(new_log, f, indent=4)
 
